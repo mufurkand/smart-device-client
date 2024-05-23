94ee98d870c70eb6ae46f8e144a3412e9ea5d9b9
mufurkand <mfurkand115@gmail.com>
2024-05-23 16:00:01 +0300

    Implement multithreading for GUI updates

--- main.py
+++ main.py
@@ -1,6 +1,79 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QHBoxLayout, QVBoxLayout, QWidget, QStatusBar, QDialog, QLineEdit, QDialogButtonBox
+from PySide6.QtCore import QRunnable, Slot, Signal, QObject, QThreadPool
 
-app = QApplication([])
+import serial
+import traceback
+import sys
+
+from time import sleep
+
+class WorkerSignals(QObject):
+  '''
+  Defines the signals available from a running worker thread.
+
+  Supported signals are:
+
+  finished
+      No data
+
+  error
+      tuple (exctype, value, traceback.format_exc() )
+
+  result
+      object data returned from processing, anything
+
+  progress
+      int indicating % progress
+
+  '''
+  finished = Signal()
+  error = Signal(tuple)
+  result = Signal(object)
+  progress = Signal(int)
+
+class Worker(QRunnable):
+  '''
+  Worker thread
+
+  Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
+
+  :param callback: The function callback to run on this worker thread. Supplied args and
+                    kwargs will be passed through to the runner.
+  :type callback: function
+  :param args: Arguments to pass to the callback function
+  :param kwargs: Keywords to pass to the callback function
+
+  '''
+
+  def __init__(self, fn, *args, **kwargs):
+    super(Worker, self).__init__()
+
+    # Store constructor arguments (re-used for processing)
+    self.fn = fn
+    self.args = args
+    self.kwargs = kwargs
+    self.signals = WorkerSignals()
+
+    # Add the callback to our kwargs
+    self.kwargs['progressCallback'] = self.signals.progress
+
+  @Slot()
+  def run(self):
+    '''
+    Initialise the runner function with passed args, kwargs.
+    '''
+
+    # Retrieve args/kwargs here; and fire processing using them
+    try:
+      result = self.fn(*self.args, **self.kwargs)
+    except:
+      traceback.print_exc()
+      exctype, value = sys.exc_info()[:2]
+      self.signals.error.emit((exctype, value, traceback.format_exc()))
+    else:
+      self.signals.result.emit(result)  # Return the result of the processing
+    finally:
+      self.signals.finished.emit()  # Done
 
 # query the user for the serial port, SSID, and password
 class UserInputDialog(QDialog):
@@ -45,23 +118,33 @@ class UserInputDialog(QDialog):
 
 # create the main window
 class MainWindow(QMainWindow):
-  def __init__(self):
+  def __init__(self, port, ssid, password):
     super().__init__()
 
     self.setWindowTitle("Smart Device Control")
+    self.resize(300, 75)
 
     self.statusBar = QStatusBar()
     self.setStatusBar(self.statusBar)
-    controls = Controls(self.statusBar)
+    self.port = port
+    self.ssid = ssid
+    self.password = password
 
-    self.setCentralWidget(controls)
+    self.threadpool = QThreadPool()
+    self.ser = None
+    self.url = ""
 
-# create the controls
-class Controls(QWidget):
-  def __init__(self, statusBar):
-    super().__init__()
+    self.statusBar.showMessage("Not connected to the device.")
+    
+    connectionControlRow = QHBoxLayout()
 
-    self.statusBar = statusBar
+    connectionControlLabel = QLabel("Connection Control:")
+    self.connectionControlButton = QPushButton("Connect")
+    self.connectionControlButton.setCheckable(True)
+    self.connectionControlButton.clicked.connect(self.handleConnectionControl)
+
+    connectionControlRow.addWidget(connectionControlLabel)
+    connectionControlRow.addWidget(self.connectionControlButton)
 
     lightControlRow = QHBoxLayout()
 
@@ -69,15 +152,54 @@ class Controls(QWidget):
 
     self.lightControlButton = QPushButton("Turn On")
     self.lightControlButton.setCheckable(True)
+    self.lightControlButton.setEnabled(False)
     self.lightControlButton.clicked.connect(self.handleLightControl)
 
     lightControlRow.addWidget(lightControlLabel)
     lightControlRow.addWidget(self.lightControlButton)
 
     layout = QVBoxLayout()
+    layout.addLayout(connectionControlRow)
     layout.addLayout(lightControlRow)
 
-    self.setLayout(layout)
+    controls = QWidget()
+    controls.setLayout(layout)
+
+    self.setCentralWidget(controls)
+    
+    self.show()
+
+  def connectToSerial(self, progressCallback):
+    while self.ser is None:
+      progressCallback.emit(0)
+      try:
+        self.ser = serial.Serial(self.port, 115200, timeout=3)
+        self.ser.open()
+      except:
+        self.statusBar.showMessage("Connection failed. retrying in 3s...")
+        self.ser = None
+        sleep(3)
+    progressCallback.emit(50)
+    # self.ser.write((self.ssid + "," + self.password).encode())
+    sleep(2)
+    progressCallback.emit(100)
+
+  def connectionComplete(self):
+    self.connectionControlButton.setText("Connected")
+    self.statusBar.showMessage("Connected to the device")
+  
+  def setProgress(self, n):
+    self.statusBar.showMessage(f"Establishing connection... {n}%")
+
+  def handleConnectionControl(self):
+    self.connectionControlButton.setEnabled(False)
+    worker = Worker(self.connectToSerial) # Any other args, kwargs are passed to the run function
+    worker.signals.finished.connect(self.connectionComplete)
+    worker.signals.progress.connect(self.setProgress)
+
+    # Execute
+    self.connectionControlButton.setText("Connecting...")
+    self.threadpool.start(worker)
 
   def handleLightControl(self, checked):
     if checked:
@@ -87,11 +209,12 @@ class Controls(QWidget):
         self.statusBar.showMessage("LED is turned off")
         self.lightControlButton.setText("Turn On")
 
+app = QApplication([])
 dialog = UserInputDialog()
-if dialog.exec() == QDialog.Accepted:
-  userInputs = dialog.getInputs()
-  print(userInputs)
-  # connect to the serial port using pyserial
-  mainWindow = MainWindow()
-  mainWindow.show()
-  app.exec()
+dialog.rejected.connect(sys.exit)
+dialog.exec()
+userInputs = dialog.getInputs()
+
+mainWindow = MainWindow(userInputs[0], userInputs[1], userInputs[2])
+
+app.exec()
